367ee5fa2fe022283cccbc16825892200b2cd9f0
Cat-Hanbit <hello.git.GitHub@gmail.com>
2022-09-04 18:57:55 +0900

    20220904-6

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,32 @@
 "use strict";
 
+const id = document.querySelector("#id"),
+    psword = document.querySelector("#psword"),
+    loginBtn = document.querySelector("#btn");
 
+var login = function login() {
+    const req = {
+        id: id.value,
+        psword: psword.value
+    };
+    console.log(req.id.length);
+    // if (id.value.length <= 0) {
+    //     alert("아이디를 입력해주세요.");
+    //     return false;
+    // }
+    // if (psword.value.length <= 0) {
+    //     alert("패스워드를 입력해주세요.");
+    //     return false;
+    // }
+    fetch("/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(req),
+    })
+
+}
+
+loginBtn.addEventListener("click", login);
 
